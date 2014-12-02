e7b5e57cffb3b9532ecd7e58d2dec89b3bb8fb94
veromeyu <veronicameyu@gmail.com>
2014-12-02 20:18:10 +0100

    ventana

--- src/util/ventanaAccion.java
+++ src/util/ventanaAccion.java
@@ -15,73 +15,111 @@ import java.util.ArrayList;
 import javax.swing.DefaultListModel;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
 public class ventanaAccion extends JFrame implements ActionListener, ComponentListener{
-	private JTable table;
+ 
+ private JTable table;
+ JPanel panelSuperior;
+ JPanel panelInferior;
+ JPanel panelCentral;
+ JPanel panelBotonera;
+ JPanel panelListas;
+ 
+ JScrollPane scrollLista; 
+ JList listaVariables;
+ 
+ JButton bActivar;
+ JButton bImagenPlaca;
+ JButton bActuar;
+ JButton bSalir;
+ JButton bBuscar;
+ JButton bListar;
+ 
+ JTextField palabraBuscar;
 
-	public ventanaAccion(ArrayList lista){
-		getContentPane().setLayout(new BorderLayout(0, 0));
-		
-		JPanel panelTabla = new JPanel();
-		getContentPane().add(panelTabla, BorderLayout.CENTER);
-		
-		table = new JTable();
-		table.setModel(this.cambiarTabla(lista));
-		panelTabla.add(table);
-		
-		JPanel panelBotonera = new JPanel();
-		getContentPane().add(panelBotonera, BorderLayout.EAST);
-		panelBotonera.setLayout(new BorderLayout(0, 0));
-		
-		JButton btnNewButton = new JButton("New button");
-		panelBotonera.add(btnNewButton, BorderLayout.CENTER);
-		
-		JButton btnNewButton_1 = new JButton("New button");
-		panelBotonera.add(btnNewButton_1, BorderLayout.SOUTH);
-		
-		JButton btnNewButton_2 = new JButton("New button");
-		panelBotonera.add(btnNewButton_2, BorderLayout.NORTH);
-		
-	}
-	@Override
-	public void componentResized(ComponentEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+ public ventanaAccion(){
+ 
+ panelSuperior = new JPanel();
+ panelInferior = new JPanel();
+ panelCentral = new JPanel();
+ panelBotonera = new JPanel();
+ panelListas = new JPanel();
+ 
+ panelBotonera.setLayout(new BorderLayout());
+ bActivar = new JButton ("On");
+ bImagenPlaca = new JButton ("Imagen Placa"); 
+ bActuar = new JButton("Actuar");
+ panelBotonera.add(bActivar,"Center");
+ panelBotonera.add(bImagenPlaca,"Center");
+ panelBotonera.add(bActuar,"Center");
+ 
+ panelInferior.setLayout(new FlowLayout());
+ panelInferior.add(palabraBuscar);
+ panelInferior.add(bBuscar);
+ panelInferior.add(bListar);
+ panelInferior.add(bSalir);
+ 
+ 
+ this.getContentPane().setLayout(new BorderLayout());
+ this.getContentPane().add(panelSuperior,"North");
+ this.getContentPane().add(panelCentral,"Center");
+ this.getContentPane().add(panelInferior,"South");
+ 
+ this.setSize(600,320);
+ this.setResizable(false);
+ this.setVisible(true);
 
-	@Override
-	public void componentMoved(ComponentEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+ bActivar.addActionListener(this);
+ bActuar.addActionListener(this);
+ bBuscar.addActionListener(this);
+ bImagenPlaca.addActionListener(this);
+ bListar.addActionListener(this);
+ bSalir.addActionListener(this);
+ 
+ 
+ 
+ }
+ @Override
+ public void componentResized(ComponentEvent e) {
+ // TODO Auto-generated method stub
+ 
+ }
 
-	@Override
-	public void componentShown(ComponentEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+ @Override
+ public void componentMoved(ComponentEvent e) {
+ // TODO Auto-generated method stub
+ 
+ }
 
-	@Override
-	public void componentHidden(ComponentEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+ @Override
+ public void componentShown(ComponentEvent e) {
+ // TODO Auto-generated method stub
+ 
+ }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	public DefaultListModel cambiarTabla(ArrayList lista) {
-		DefaultListModel a = new DefaultListModel();
-		
-		for (int i = 0; i<=lista.size();i++ ){
-			a.add(i, lista.get(i));
-		}	
-		
-		return a;
-	}
+ @Override
+ public void componentHidden(ComponentEvent e) {
+ // TODO Auto-generated method stub
+ 
+ }
+
+ @Override
+ public void actionPerformed(ActionEvent e) {
+ // TODO Auto-generated method stub
+ 
+ }
+ public DefaultListModel cambiarTabla(ArrayList lista) {
+ DefaultListModel a = new DefaultListModel();
+ 
+ for (int i = 0; i<=lista.size();i++ ){
+ a.add(i, lista.get(i));
+ } 
+ 
+ return a;
+ }
 }
