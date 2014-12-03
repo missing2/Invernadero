4275aedd518e1176d9416e6e5d2af43315ae8caf
veromeyu <veronicameyu@gmail.com>
2014-12-03 11:35:29 +0100

    ventana, falta agregar la tabla y los metodos al escuchador

--- src/util/ventanaAccion.java
+++ src/util/ventanaAccion.java
@@ -8,6 +8,7 @@ import java.awt.event.ComponentListener;
 import javax.swing.JFrame;
 import javax.swing.JList;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.BorderLayout;
 import java.util.ArrayList;
@@ -24,7 +25,7 @@ import javax.swing.table.TableModel;
 public class ventanaAccion extends JFrame implements ActionListener, ComponentListener{
  
  private JTable table;
- JPanel panelSuperior;
+
  JPanel panelInferior;
  JPanel panelCentral;
  JPanel panelBotonera;
@@ -44,19 +45,25 @@ public class ventanaAccion extends JFrame implements ActionListener, ComponentLi
 
  public ventanaAccion(){
  
- panelSuperior = new JPanel();
  panelInferior = new JPanel();
  panelCentral = new JPanel();
  panelBotonera = new JPanel();
  panelListas = new JPanel();
  
- panelBotonera.setLayout(new BorderLayout());
+ palabraBuscar = new JTextField(10);
+ 
  bActivar = new JButton ("On");
  bImagenPlaca = new JButton ("Imagen Placa"); 
  bActuar = new JButton("Actuar");
- panelBotonera.add(bActivar,"Center");
- panelBotonera.add(bImagenPlaca,"Center");
- panelBotonera.add(bActuar,"Center");
+ bBuscar = new JButton("Buscar");
+ bListar = new JButton("Listado");
+ bSalir = new JButton("Salir");
+ 
+ panelBotonera.setLayout(new FlowLayout());
+ panelBotonera.add(bActivar);
+ panelBotonera.add(bImagenPlaca);
+ panelBotonera.add(bActuar);
+
  
  panelInferior.setLayout(new FlowLayout());
  panelInferior.add(palabraBuscar);
@@ -64,9 +71,11 @@ public class ventanaAccion extends JFrame implements ActionListener, ComponentLi
  panelInferior.add(bListar);
  panelInferior.add(bSalir);
  
+ panelCentral.add(panelListas, "Center");
+ 
  
  this.getContentPane().setLayout(new BorderLayout());
- this.getContentPane().add(panelSuperior,"North");
+ this.getContentPane().add(panelBotonera,"North");
  this.getContentPane().add(panelCentral,"Center");
  this.getContentPane().add(panelInferior,"South");
  
@@ -80,10 +89,25 @@ public class ventanaAccion extends JFrame implements ActionListener, ComponentLi
  bImagenPlaca.addActionListener(this);
  bListar.addActionListener(this);
  bSalir.addActionListener(this);
- 
- 
- 
+ setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+
  }
+ private boolean enActivado = false;
+
+	// Método privado utilizado para activar o desactivar los componentes de acuerdo al modo de activación
+	private void setModoActivado( boolean on ) {
+		enActivado = on;
+		bActuar.setEnabled( !on );
+		bImagenPlaca.setEnabled( !on );
+		bBuscar.setEnabled( !on );
+		bListar.setEnabled( !on );
+		listaVariables.setEnabled( !on );
+		//nick.setEnabled( on );
+		if (on)
+			bActivar.setText( "OFF" );
+		else
+			bActivar.setText( "ON" );
+	}
  @Override
  public void componentResized(ComponentEvent e) {
  // TODO Auto-generated method stub
@@ -111,6 +135,26 @@ public class ventanaAccion extends JFrame implements ActionListener, ComponentLi
  @Override
  public void actionPerformed(ActionEvent e) {
  // TODO Auto-generated method stub
+	 if(e.getSource().equals(bActivar)){
+		 if(!enActivado){
+			 setModoActivado(true);
+			 //aqui cambiar el valor de la tabla a off
+		 }else{
+			 //aqui a on
+		 }
+		 
+	 }else if(e.getSource().equals(bActuar)){
+		 
+	 }else if(e.getSource().equals(bBuscar)){
+		 
+	 }else if(e.getSource().equals(bImagenPlaca)){
+		 
+	 }else if(e.getSource().equals(bListar)){
+		 
+	 }else if(e.getSource().equals(bSalir)){
+		 System.exit(0);
+	 }
+
  
  }
  public DefaultListModel cambiarTabla(ArrayList lista) {
@@ -122,4 +166,10 @@ public class ventanaAccion extends JFrame implements ActionListener, ComponentLi
  
  return a;
  }
+ public static void main(String[] args)
+ {
+	ventanaAccion v= new ventanaAccion();
+	v.setVisible(true);
+ }
+
 }
